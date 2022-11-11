08a2463e001301af63336f35a1c871b040db3388
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-11-11 16:05:33 +0530

    Created The Window.

---
+++ window.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    autoHideMenuBar: true
+  })
+
+  win.loadFile('./Templates/home.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
